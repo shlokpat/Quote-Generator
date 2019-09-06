5e764f28775aa25e4bdd35bfba7d5e300f8a15a4
Shlok <46587444+shlokpat@users.noreply.github.com>
2019-09-06 16:59:45 -0400

    improvements to DOM

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var quoteDiv = document.getElementById('root')
+var quoteDiv = document.getElementById('quote')
 
 var request = new XMLHttpRequest()
 
@@ -7,18 +7,30 @@ request.open('GET', 'https://quotes.rest/qod', true)
 request.onload = function() {
     var quoteData = JSON.parse(request.responseText)
     
+    renderHTML(quoteData)
+
     console.log(quoteData)
 
     console.log(quoteData.contents.quotes[0].quote)
 
     console.log(quoteData.contents.quotes[0].author)
 
-    var quote = quoteData.contents.quotes[0].quote
-    var author = quoteData.contents.quotes[0].author
 
-    quoteDiv.insertAdjacentHTML('beforeend', quote)
-    quoteDiv.insertAdjacentHTML('afterEnd', author)
 
 }
 
 request.send()
+
+function renderHTML(data) {
+    var quote = data.contents.quotes[0].quote
+    var author = data.contents.quotes[0].author
+
+    var quoteText = document.createElement('h2')
+    quoteText.innerHTML = '"' + quote + '"'
+    var authorText = document.createElement('h2')
+    authorText.innerHTML = '- ' + author
+
+    quoteDiv.appendChild(quoteText)
+    quoteDiv.appendChild(authorText)
+    
+}
